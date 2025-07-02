c77713d68d3311ca4f9330fe3c3da2770be9b5a1
Atharva Jagtap <atharvacjagtap2005@gmail.com>
2025-07-02 23:56:41 +0530

    added

--- src/customer/HomeCarosal/MainCaroselData.jsx
+++ src/customer/HomeCarosal/MainCaroselData.jsx
@@ -1,14 +1,14 @@
 export const mainCarouselData = [
   {
-    image: "https://rangita.com/cdn/shop/files/image_56_9.png?v=1716531786&width=2000",
+    image: "https://rangita.com/cdn/shop/articles/mothers_day_blog_banner_607bbee6-4a2f-4c3a-89d7-817ef3d82af3.png?v=1746784378&width=1780",
     path: "/women/clothing/women_dress"
   },
   {
-    image: "https://rangita.com/cdn/shop/files/Frame_1359318159_1.png?v=1716532341&width=2000",
+    image: "https://rangita.com/cdn/shop/articles/blog_banner_5.jpg?v=1751451420&width=1780",
     path: "/women/clothing/women_dress"
   },
   {
-    image: "https://rangita.com/cdn/shop/articles/short_kurti_blog_1.png?v=1751277248&width=1780",
+    image: "https://rangita.com/cdn/shop/articles/short_kurti_blog_1.png?v=1751277248&width=2000",
     path: "/women/clothing/women_dress"
   },
  
