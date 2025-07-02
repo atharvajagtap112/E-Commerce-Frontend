3225de393a094c322de69d813ebd404368d68c31
Atharva Jagtap <atharvacjagtap2005@gmail.com>
2025-07-02 23:39:26 +0530

    added

--- src/customer/HomeCarosal/MainCaroselData.jsx
+++ src/customer/HomeCarosal/MainCaroselData.jsx
@@ -1,14 +1,14 @@
 export const mainCarouselData = [
   {
-    image: "https://firebasestorage.googleapis.com/v0/b/flutterapp-649f8.appspot.com/o/Banners%2FImages%2FFrame%203.png?alt=media&token=758c8406-3372-4a34-b359-00dca8dfd9be",
+    image: "https://rangita.com/cdn/shop/files/image_56_9.png?v=1716531786&width=2000",
     path: "/women/clothing/women_dress"
   },
   {
-    image: "https://firebasestorage.googleapis.com/v0/b/flutterapp-649f8.appspot.com/o/Banners%2FImages%2FFrame%204.png?alt=media&token=5c791e11-610e-4e09-af87-9a243797e384         ",
+    image: "https://rangita.com/cdn/shop/files/Frame_1359318159_1.png?v=1716532341&width=2000",
     path: "/women/clothing/women_dress"
   },
   {
-    image: "https://firebasestorage.googleapis.com/v0/b/flutterapp-649f8.appspot.com/o/Banners%2FImages%2FFrame%205.png?alt=media&token=6f7289e0-89d6-4649-af1b-9eeb5c6b029b",
+    image: "https://rangita.com/cdn/shop/articles/short_kurti_blog_1.png?v=1751277248&width=1780",
     path: "/women/clothing/women_dress"
   },
  
